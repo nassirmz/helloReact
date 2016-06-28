5f824dd8cc1deab7a8eaedeefc5b6323a5072226
nassir <nassirmz@gmail.com>
2016-06-28 12:17:59 -0700

    (feat) Add greeterForm component

---
+++ public/app.jsx
@@ -0,0 +1,4 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var GreeterMessage = require('./components/GreeterMessage');
+var GreeterForm = require('./components/GreeterForm');
