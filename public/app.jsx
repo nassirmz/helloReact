6eb5e1489b35eb2dda42ccdaf3ecb3ee66750789
nassir <nassirmz@gmail.com>
2016-06-28 12:24:26 -0700

    (feat) Add Greeter container component

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var GreeterMessage = require('./components/GreeterMessage');
-var GreeterForm = require('./components/GreeterForm');
+var Greeter = require('./components/Greeter');
+
+ReactDOM.render(
+  <Greeter/>,
+  document.getElementById('app')
+);
--- public/components/Greeter.jsx
+++ public/components/Greeter.jsx
@@ -1,5 +1,34 @@
-function greeter () {
-  document.write('From greeter function ');
-}
+var React = require('react');
+var ReactDOM = require('react-dom');
+var GreeterMessage = require('./GreeterMessage');
+var GreeterForm = require('./GreeterForm');
 
-module.exports = greeter;
+var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'This is the default message'
+    };
+  },
+  getInitialState: function () {
+    return {
+      name: this.props.name,
+      message: this.props.message
+    }
+  },
+  handleNewName: function (updates) {
+    this.setState(updates);
+  },
+  render: function () {
+    var name = this.state.name;
+    var message = this.state.message;
+    return (
+      <div>
+        <GreeterMessage name={name} message={message}/>
+        <GreeterForm onNewName={this.handleNewName}/>
+      </div>
+    );
+  }
+});
+
+module.exports = Greeter;
